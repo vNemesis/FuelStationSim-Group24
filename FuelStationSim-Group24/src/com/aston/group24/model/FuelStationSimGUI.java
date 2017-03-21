98831e9e98c85cdc5d87cb9c3cc37dd413cc71d9
HarmanU <uppalhrs@aston.ac.uk>
2017-03-21 20:48:46 +0000

    GUI Skeleton

---
+++ FuelStationSim-Group24/src/com/aston/group24/model/FuelStationSimGUI.java
@@ -0,0 +1,34 @@
+package com.aston.group24.model;
+
+import javax.swing.JFrame;
+
+public class FuelStationSimGUI {
+	
+	private JFrame mainFrame;
+	
+	public FuelStationSimGUI(Simulation s)
+	{
+				// Step 1: create the components
+
+				
+				// Step 2: Set the properties of the components
+
+				
+				// Step 3: Create containers to hold the components
+
+				
+				// Step 4: Specify LayoutManagers
+
+				
+				// Step 5: Add components to containers 
+
+				
+				// Step 6: Arrange to handle events in the user interface
+				
+				
+				// Step 7: Display the GUI
+				mainFrame.pack();
+				mainFrame.setVisible(true);
+	}
+
+}
