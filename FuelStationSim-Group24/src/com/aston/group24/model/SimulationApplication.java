aa87f11dac9e1b03863c3ad6501a344a7a2b4410
HarmanU <uppalhrs@aston.ac.uk>
2017-04-01 19:36:45 +0100

    Updated method to account for more parameters

--- FuelStationSim-Group24/src/com/aston/group24/model/SimulationApplication.java
+++ FuelStationSim-Group24/src/com/aston/group24/model/SimulationApplication.java
@@ -16,7 +16,7 @@ public class SimulationApplication {
 	public static void main(String[] args)
 	{ 
 		// create sim with default values
-		Simulation s = new Simulation(3, 2, 10);
+		Simulation s = new Simulation(3, 2, 0.5, 0.5, 10);
 		
 		@SuppressWarnings("unused") FuelStationSimGUI fsg = new FuelStationSimGUI(s);
 	}
