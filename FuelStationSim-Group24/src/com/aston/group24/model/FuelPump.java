f764304c42a44f8c64bfeacdf3b87f2cd8585df2
HarmanU <uppalhrs@aston.ac.uk>
2017-04-01 19:35:11 +0100

    Updated methods for people not vehicles

--- FuelStationSim-Group24/src/com/aston/group24/model/FuelPump.java
+++ FuelStationSim-Group24/src/com/aston/group24/model/FuelPump.java
@@ -1,7 +1,8 @@
 package com.aston.group24.model;
 
 import java.util.ArrayList;
-import com.aston.group24.vehicles.Vehicle;;
+
+import com.aston.group24.people.Person;;
 
 public class FuelPump {
 	
@@ -10,7 +11,7 @@ public class FuelPump {
 	private double CSA; //Current space Available
 	private int fuelSupply;
 	private int pumpID;
-	private ArrayList<Vehicle> currentCars;
+	private ArrayList<Person> currentPeople;
 	private int fueledThisTick;
 	
 	public FuelPump(int pumpID, double space, int fuelSupply)
@@ -18,7 +19,7 @@ public class FuelPump {
 		this.pumpID = pumpID;
 		this.space = space;
 		this.fuelSupply = fuelSupply;
-		currentCars = new ArrayList<Vehicle>();
+		currentPeople = new ArrayList<Person>();
 		CSA = space;
 		CSO = 0;
 	}
@@ -26,11 +27,18 @@ public class FuelPump {
 	/*
 	 * Add vehicle to pumps list
 	 */
-	protected void addVehicle(Vehicle v)
+	protected void addPerson(Person p)
 	{
-		currentCars.add(v);
-		CSO += v.getSize();
+		currentPeople.add(p);
+		CSO += p.getVehicle().getSize();
 		CSA = space - CSO;
+	}
+	
+	protected void removePerson(Person p)
+	{
+		currentPeople.remove(p);
+		CSO -= p.getVehicle().getSize();
+		CSA = space + CSO;
 	}
 	
 	protected int getID()
@@ -54,16 +62,22 @@ public class FuelPump {
 		return space;
 	}
 	
+	protected ArrayList<Person> getPeople()
+	{
+		return currentPeople;
+	}
+	
 	/*
 	 * supply fuel to all cars at the pump
 	 */
 	protected void supplyFuel()
 	{
-		if (currentCars.size() != 0)
+		if (currentPeople.size() != 0)
 		{
-			for(Vehicle v : currentCars)
+			for(Person p : currentPeople)
 			{
-				v.fillCar(fuelSupply);
+				if(p.getRefuelled() == false)
+				p.getVehicle().fillCar(fuelSupply);
 				fueledThisTick++;
 			}
 		}
