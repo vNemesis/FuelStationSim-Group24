ef02b81c70b46249e628bfb7b52f39695aff9207
HarmanU <uppalhrs@aston.ac.uk>
2017-04-30 00:17:54 +0530

    Updated Test to be more informative

--- FuelStationSim-Group24/src/com/aston/group24/model/FuelStationTest.java
+++ FuelStationSim-Group24/src/com/aston/group24/model/FuelStationTest.java
@@ -1,7 +1,5 @@
 package com.aston.group24.model;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,24 +17,24 @@ public class FuelStationTest {
 	public void setUp() throws Exception {
 		
 		fs = new FuelStation(3, 2);
-		sc1 = new SmallCarDriver(6); 		//size 1.5
-		sc2 = new SmallCarDriver(7); 		//size 1
-		sc3 = new SmallCarDriver(8); 		//size 1.5
+		sc1 = new SmallCarDriver(6); 		//size 1.0
+		sc2 = new SmallCarDriver(7); 		//size 1.0
+		sc3 = new SmallCarDriver(8); 		//size 1.0
 		sc4 = new SmallCarDriver(9); 		//size 1.0
 	}
 
 	@Test
 	public void test() 
 	{
-
-		fs.testing(sc1, sc2, sc3);
+		System.out.println("This Test will create 3 Pumps, Sort them and then Print them to console");
+		System.out.println("Number of Pumps: " + fs.numberOfPumps());
+		fs.testing(sc1, sc2, sc3); // test with 3 small cars
 		
 		FuelPump pumpWithMostSpace = fs.pumpWithShortestLine(sc4); //find pump with space for sc4
 		
-		System.out.println(fs.numberOfPumps());
-		System.out.println(fs.pumpsCSA());
+		//System.out.println("Pump" + fs.pumpsCSA());
 		
-		System.out.println(pumpWithMostSpace.getID()); // ID of pump with most space
+		System.out.println("Pump number with the most space before ordering (Pump Id's Start at 0): " + pumpWithMostSpace.getID()); // ID of pump with most space
 	}
 
 }
