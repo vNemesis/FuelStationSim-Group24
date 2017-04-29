ef40aecb67c2defcbb0f78dc67feda9ea0ab95e1
HarmanU <uppalhrs@aston.ac.uk>
2017-04-30 00:19:59 +0530

    Added Logger Class and util Package
    Logger class will be used to enforce classes to log data.
    
    util package for utilitles we create / use

---
+++ FuelStationSim-Group24/src/com/aston/group24/util/Logger.java
@@ -0,0 +1,12 @@
+package com.aston.group24.util;
+
+public interface Logger {
+	
+	/**
+	 * Used to enforce classes that should log information
+	 */
+	public static String getLog()
+	{
+		return null;
+	}
+}
