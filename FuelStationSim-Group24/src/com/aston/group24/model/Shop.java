9071c820b49da65848d65460296afdb9b91d54dc
JakeTurner96 <jw_turner@hotmail.co.uk>
2017-05-01 14:54:00 +0100

    Update Shop.java

--- FuelStationSim-Group24/src/com/aston/group24/model/Shop.java
+++ FuelStationSim-Group24/src/com/aston/group24/model/Shop.java
@@ -3,99 +3,117 @@ package com.aston.group24.model;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Random;
 
 import com.aston.group24.people.Person;
 
+/**
+ * Shop
+ * - Will contain and manage tills and shop floor.
+ * 
+ * @version 0.0.3
+ * @ JTurner
+ */
+
 public class Shop{
 
-	private HashMap<Person, Double> shopTime;
-	private HashMap<Person, Double> tillTime;
+	private HashMap<Person, Integer> shopTime; //HashMap for storing and managing the time spent on the shop floor.
+	private HashMap<Person, Integer> tillTime; //HashMap for storing and managing the time spent paying at the till.
 	private ArrayList<Person> shopFloor; //Array for storing people currently on the shop floor.
 	private ArrayList<Till> tills; //Array for storing tills.
 
 public Shop(int numTills)
   {	
-	shopTime = new HashMap<Person, Double>();
-	tillTime = new HashMap<Person, Double>();
-    tills = new ArrayList<Till>();
+	shopTime = new HashMap<Person, Integer>();
+	tillTime = new HashMap<Person, Integer>();
+    	tills = new ArrayList<Till>();
  
     createTills(numTills);
   }
-
-  protected void rndShopTime(Person p)
-  {
-	  Random r = new Random(2-3);
-	  Double time = r.nextDouble();
-	  
-	  shopTime.put(p, time);  
-	
-  }
   
-  protected void rndTillTime(Person p)
-  {
-	  Random r = new Random(2-3);
-	  Double time = r.nextDouble();
-	  
-	  tillTime.put(p, time);  
-  } 
-  
-  
-
+  /**
+   * Create X number of tills in the shop
+   */
   protected void createTills(int ammountOfTills)
   {
       for(int i = 0; i < ammountOfTills; i++)
       {
           tills.add(new Till());
-
       }
   }
  
+  /**
+   * Add specified person p to shop floor
+   */
   protected void addPersonToFloor(Person p)
-  {		   	   
+  {		   	
 	  shopFloor.add(p);
-	  
+	  //Add person p to shopTime HashMap and set time to 0
+	  shopTime.put(p, 0);
   }
   
+  /**
+   * Add person p to till with shortest queue
+   */
   protected boolean addPersonToTills(Person p)
-  {
+  {		
 	  Till validTill = tillWithShortesQueue();
 	
 	  if(validTill != null)
 	  {
 		  tillWithShortesQueue().addPerson(p);
+		  //Add person p to tillTime HashMap and set time to 0
+		  tillTime.put(p, 0);
 		  return true;
 	  }
 	  else 
 	  {
 		  return false;
-	  }
-	  
+	  } 
   }
   
-  protected void removePersonFromTills(Person p, Till t)
+  /**
+   * Remove person from till
+   */
+  protected void removePersonFromTills(Person p)
   {
-	  t.removePerson(p);
+	  for(int i = 0; i < tills.size(); i++)
+	  {
+		  if(tills.get(i).containsPerson(p))
+		  {
+			  tills.get(i).removePerson(p);
+		  }  
+	  }
   }
   
+  /**
+   * Remove person from shopFloor
+   */
   protected void removePersonBrowsing(Person p)
   {
-	  shopFloor.remove(p);
-	  
+	  shopFloor.remove(p);  
   }
   
-  //TODO Needs to return a list of the people that have finished, at the moment just returns the one person in the parameter
-  protected ArrayList<Person> getFinishedBrowsing(Person p)
+  /**
+   * Return ArrayList of people who have finished browsing
+   */
+  protected ArrayList<Person> getFinishedBrowsing()
   {
 	 ArrayList<Person> finishedBrowsing = new ArrayList<Person>();
 
-		 if(p.getShopTime() == shopTime.get(p))
-		 {
-			 finishedBrowsing.add(p);
+	 for(int i = 0; i < shopFloor.size(); i++)
+	 { 
+		 if(shopFloor.get(i).getShopTime() == shopTime.get(shopFloor.get(i)))
+		 {		 
+			 
+			 finishedBrowsing.add(shopFloor.get(i));
 		 }
-		 return finishedBrowsing;
-  }
+	 }
+	 return finishedBrowsing;
+  } 
   
+  /**
+   * Return ArrayList of people who have finished paying
+   */
   protected ArrayList<Person> getFinishedPaying(Person p)
   {
 	  ArrayList<Person> finishedPaying = new ArrayList<Person>();
@@ -104,14 +122,15 @@ public Shop(int numTills)
 	  {
 		  finishedPaying.add(p);
 	  }
-	  return finishedPaying;
-	  
+	  return finishedPaying;  
   }
  
-  
+  /**
+   * Sort tills in descending order
+   */
   protected Till tillWithShortesQueue()
   {  
-	  
+	 //ArryList to store sorted tills 
 	 ArrayList<Integer> sortedTills = new ArrayList<Integer>();
 	 
 	 for(Till t : tills)
@@ -120,16 +139,19 @@ public Shop(int numTills)
 		
 	 }
 	 
-	 Collections.sort(sortedTills, Collections.reverseOrder());
+	 Collections.sort(sortedTills, Collections.reverseOrder());//Sort values
 	  
 	 for(Till t : tills)
 		 if(t.queueLength() == sortedTills.get(0))
 		 {
 			 return t;
 		 }
-	return null;
-	  
+	return null;	  
   }
   
-   
+  public void simulate()
+  {
+
+	 
+  }  
 }
