e1ca18de7c557db54a6efebe451318d16771d7fd
JakeTurner96 <jw_turner@hotmail.co.uk>
2017-05-02 16:27:37 +0100

    Update Till.java

--- FuelStationSim-Group24/src/com/aston/group24/model/Till.java
+++ FuelStationSim-Group24/src/com/aston/group24/model/Till.java
@@ -4,9 +4,18 @@ import java.util.Queue;
 
 import com.aston.group24.people.Person;;
 
+/**
+ * Till
+ * 
+ * Models a till
+ * 
+ * @author JTurner
+ * 
+ */
+
 public class Till{
 
-private Queue<Person> tillQueue; 
+private Queue<Person> tillQueue;
 	
 	
     public Till()
@@ -14,21 +23,33 @@ private Queue<Person> tillQueue;
         tillQueue = new LinkedList<Person>();
     }
 
+    /**
+     * Add a person at the back of the till queue
+     */
     protected void addPerson(Person p)
     {
         tillQueue.add(p);
     }
-
+    
+    /**
+     * Remove the person at the front of the till queue
+     */
     protected void removePerson(Person p)
     {
         tillQueue.remove(p);
     }
 
+    /**
+     * Return the till queue size
+     */
     protected int queueLength()
     {
         return tillQueue.size();
     }
     
+    /**
+     * Return true if the till queue contains person p or false if not
+     */
     protected boolean containsPerson(Person p)
     {
     	if(tillQueue.contains(p))
@@ -37,6 +58,14 @@ private Queue<Person> tillQueue;
     	}
     	else return false;
     	
+    }
+    
+    /**
+     * Returns the person at the front of the queue
+     */
+    protected Person getFirstInQueue()
+    {
+    	return tillQueue.peek();
     }
 
 }
