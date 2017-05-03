3900c16a062234b914c7b300745555896b4750a6
Harman Uppal <Harman Uppal@DESKTOP-HARMAN>
2017-05-03 20:21:04 +0100

    added validation of arguemnts

--- FuelStationSim-Group24/src/com/aston/group24/view/SimulationApplication.java
+++ FuelStationSim-Group24/src/com/aston/group24/view/SimulationApplication.java
@@ -32,25 +32,148 @@ public class SimulationApplication {
 			@SuppressWarnings("unused") FuelStationSimGUI fsg = new FuelStationSimGUI(s);
 		}
 		else if (args.length == 8)
-		{
-			// Pass arguments to simulation
-			Simulation s = new Simulation(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Double.parseDouble(args[2]),  Double.parseDouble(args[3]), Boolean.parseBoolean(args[4]), Long.parseLong(args[5]));
-			s.runSim(Integer.parseInt(args[6]));
-			
-			if(Boolean.parseBoolean(args[7]))
+		{	
+			if (!validateArguments(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]))
 			{
-				s.PrintOutputToFile("Simulation Output");
+				System.out.print("Arguments Invalid. Try again");
 			}
-			
-			System.out.print(s.reportStartStatus());
-			System.out.print(s.reportStats());
-			
+			else
+			{
+				// Pass arguments to simulation
+				Simulation s = new Simulation(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Double.parseDouble(args[2]),  Double.parseDouble(args[3]), Boolean.parseBoolean(args[4]), Long.parseLong(args[5]));
+				s.runSim(Integer.parseInt(args[6]));
+				
+				if(Boolean.parseBoolean(args[7]) == true)
+				{
+					s.PrintOutputToFile("Simulation Output");
+				}
+				
+				System.out.print(s.reportStartStatus());
+				System.out.print(s.reportStats());
+			}
 		}
 		else
 		{
 			System.out.println("Invalid Number of arguemnts given. either 0 or 7");
 			System.out.println("Format is (int numOfPumps, int numOfTills, double probabilityP, double probabilityQ, boolean trucksEnabled, long seed, int ticksToRunSim, boolean printOutputToFile)");
 		}
+	}
+	
+	/**
+	 * Method to test whether the data entered is valid
+	 * @param numPumps Check number of pumps
+	 * @param numTills Check number of tills
+	 * @param probP Check Prob of P
+	 * @param probQ Check Prob of Q
+	 * @param seed Check seed for simulation
+	 * @param ticks Check number of Ticks to run simulation
+	 */
+	private static boolean validateArguments(String numPumps, String numTills, String probP, String probQ, String trucks, String seed, String ticks, String print)
+	{
+		 // check num of pumps is valid
+		 try { 
+		        Integer.parseInt(numPumps); 
+		    } catch(NumberFormatException e) {
+		    	System.out.print("Number of pumps is not a valid Integer");
+		    	System.out.print("\n");
+		        return false; 
+		    } catch(NullPointerException e) {
+		    	System.out.print("Number of Pumps is null! set an Integer value");
+		    	System.out.print("\n");
+		        return false;
+		    }
+		 
+		 // check number of tills is valid
+		 try { 
+		        Integer.parseInt(numTills); 
+		    } catch(NumberFormatException e) {
+		    	System.out.print("Number of tills is not a valid Integer");
+		    	System.out.print("\n");
+		        return false; 
+		    } catch(NullPointerException e) {
+		    	System.out.print("Number of tills is null! set an Integer value");
+		    	System.out.print("\n");
+		        return false;
+		    }
+		 
+		 
+		 // check seed is valid
+		 try { 
+		        Integer.parseInt(seed); 
+		    } catch(NumberFormatException e) {
+		    	System.out.print("Number of ticks is not a valid Integer");
+		    	System.out.print("\n");
+		        return false; 
+		    } catch(NullPointerException e) {
+		    	System.out.print("Number of ticks is null! set an Integer value");
+		    	System.out.print("\n");
+		        return false;
+		    }
+		 
+		 // check probability of p is valid
+		 try { 
+		        Double.parseDouble(probP); 
+		    } catch(NumberFormatException e) {
+		    	System.out.print("Number of ticks is not a valid double value (i.e 0.2)");
+		    	System.out.print("\n");
+		        return false; 
+		    } catch(NullPointerException e) {
+		    	System.out.print("Number of ticks is null! set an Integer value");
+		    	System.out.print("\n");
+		        return false;
+		    }
+		 
+		 if (Double.parseDouble(probP) < 0 || Double.parseDouble(probP) > 1)
+		 {
+			 System.out.print("Probibility of P needs to be between 0 and 1");
+		     System.out.print("\n");
+		     return false; 
+		 }
+		 
+		 // check probability of q is valid
+		 try { 
+		        Double.parseDouble(probQ); 
+		    } catch(NumberFormatException e) {
+		    	System.out.print("Number of ticks is not a valid double value (i.e 0.2)");
+		    	System.out.print("\n");
+		        return false; 
+		    } catch(NullPointerException e) {
+		    	System.out.print("Number of ticks is null! set an double value");
+		    	System.out.print("\n");
+		        return false;
+		    }
+		 
+		 if (Double.parseDouble(probQ) < 0 || Double.parseDouble(probQ) > 1)
+		 {
+			 System.out.print("Probibility of Q needs to be between 0 and 1");
+		     System.out.print("\n");
+		     return false; 
+		 }
+		 
+		 //Check ticks value is valid 
+		 try { 
+		        Integer.parseInt(ticks); 
+		    } catch(NumberFormatException e) {
+		    	System.out.print("Number of ticks is not a valid Integer");
+		    	System.out.print("\n");
+		        return false; 
+		    } catch(NullPointerException e) {
+		    	System.out.print("Number of ticks is null! set an Integer");
+		    	System.out.print("\n");
+		        return false;
+		    }
+		 
+		 if(!Boolean.parseBoolean(trucks))
+		 {
+			 return false;
+		 }
+		 if(!Boolean.parseBoolean(print))
+		 {
+			 return false;
+		 }
+		 
+		 // Return true is data is valid
+		 return true;
 	}
 
 }
